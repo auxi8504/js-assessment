2b2f6c843e1166a84f46c44574a19748964b2cc3
Auxiliadora Martinez Martinez <amartinez@growthaccelerationpartners.com>
2015-09-03 09:25:31 -0600

    Solving count exercises

--- app/count.js
+++ app/count.js
@@ -2,6 +2,28 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var count = 0;
+    var timeout;
+    var output = function (value, count) {
+      timeout = setTimeout(function () {
+        console.log(value);
+      }, count * 100);
+    };
 
+    while (start <= end) {
+      if (count === 0) {
+        console.log(start);
+      } else {
+        output(start, count);
+      }
+      ++start;
+      ++count;
+    }
+
+    return {
+      cancel: function () {
+        clearTimeout(timeout);
+      }
+    }
   }
 };
