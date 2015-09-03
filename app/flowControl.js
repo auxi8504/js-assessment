410809c144d58030e91ee181476afab1b802261d
Auxiliadora Martinez Martinez <amartinez@growthaccelerationpartners.com>
2015-09-03 10:06:07 -0600

    Solving flow control exercises

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,7 +1,7 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.flowControlAnswers = {
-  fizzBuzz : function(num) {
+  fizzBuzz: function (num) {
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
@@ -10,6 +10,20 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
+    var output = "";
+
+    if (isNaN(parseInt(num, 10))) {
+      return output;
+    }
+
+    if (num % 3 === 0) {
+      output += "fizz";
+    }
+    if (num % 5 === 0) {
+      output += "buzz";
+    }
+
+    return output || num;
 
   }
 };
